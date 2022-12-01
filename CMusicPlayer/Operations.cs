e4d49a7b6dcf78587c7042329cf080307a180e72
Ebube4im Anya <ebube4im@gmail.com>
2022-12-01 23:52:47 +0100

    Add project files.

---
+++ CMusicPlayer/Operations.cs
@@ -0,0 +1,68 @@
+﻿namespace CMusicPlayer
+{
+    internal class Operations
+    {
+
+        public static void Run()
+        {
+            Console.WriteLine("Welcome to 4im CMusic Player");
+            Console.WriteLine("Kindly Select the operation you want to run. \n 1: VIew All Songs  \n 2: Add Song  \n 3: Create PlayList  \n 4: View list of PlayList");
+            int selection = 0;
+            Song song = new Song();
+
+            CheckForDigits:
+            if (int.TryParse(Console.ReadLine(), out selection))
+            {
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("You have selected View All Songs");
+                        song.ListAllSongs();
+                        break;
+                    case 2:
+                         
+                        Console.WriteLine("You have selected Add Song");
+                        song.AddSong(AddSong());
+                        Console.WriteLine("Song have been added successfully");
+                        song.ListAllSongs();
+                        break;
+                    case 3: // View List of Songs in playlist
+                        Console.WriteLine("You have selected 3");
+                        break;
+                    case 4:
+                        //Edit Songs
+                        Console.WriteLine("You have selected 4");
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("The selected inpput is not a digit. Enter a suitable numer from the menu");
+                goto CheckForDigits;
+            }
+
+        }
+
+
+
+        public static Song AddSong()
+        {
+            Console.WriteLine("Enter the Name of the Song");
+            string name = Console.ReadLine();
+            if (!(name is not null))
+            {
+                Console.WriteLine("The input is not valid. Enter a Valid name of Song");
+            }
+
+            return new Song() { Name = name, Id = 6 };
+        }
+
+
+
+    }
+}
---
+++ CMusicPlayer/Song.cs
@@ -0,0 +1,51 @@
+﻿namespace CMusicPlayer
+{
+    internal class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Duration { get; set; }
+        public string Artist { get; set; }
+
+        public static List<Song> Songs { get; set; } = new List<Song>()
+            {
+                new Song(){ Id = 1, Name = "You Covered me", Artist="Tye Tribbett", Description = "You covered me", Duration = 2.20m},
+                 new Song(){ Id = 2, Name = "Heaven at last", Artist="Donnie MCklurkins", Description = "You covered me", Duration = 4.20m},
+                  new Song(){ Id = 3, Name = "You have done enough", Artist="Mercy Chinwo", Description = "You covered me", Duration = 3.20m},
+                   new Song(){ Id = 4, Name = "Omemma", Artist="JudiKay", Description = "You covered me", Duration = 1.20m},
+                    new Song(){ Id = 5, Name = "What did I do", Artist="GUC", Description = "You covered me", Duration = 2.20m}
+            };
+
+
+        public void Play(Song song)
+        {
+            Console.WriteLine($"Playing {song.Name}");
+            song.Play(song);
+
+        }
+
+
+
+
+        public void ListAllSongs()
+        {
+            foreach (Song song in Songs)
+            {
+                Console.WriteLine($" ID: {song.Id} | Name: {song.Name}");
+            }
+        }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+        }
+
+        public void RemoveSong(Song song)
+        {
+            Songs.Remove(song);
+        }
+
+
+    }
+}
